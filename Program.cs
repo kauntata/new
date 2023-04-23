1b7e4d19cf09c00a429ea432b64c993a2090d1b9
unknown <kauntata@mail.ru>
2023-04-23 11:16:59 +0400

    6d

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите натуральное число больше 1:");
+int number = int.Parse(Console.ReadLine());
+
+///Метод вывода натуральных чисел от N до 1:
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    NumberCounter (number - 1);
+}
+
+NumberCounter(number);
